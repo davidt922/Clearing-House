64b9d45beb75299c9a33931beeda7ebd514b9b7d
David <32110291+davidt922@users.noreply.github.com>
2018-06-21 15:58:50 +0200

    Improve comments structure

--- DAPPS/CompensationChamber/Solidity/CompensationChamber.sol
+++ DAPPS/CompensationChamber/Solidity/CompensationChamber.sol
@@ -1,40 +1,44 @@
 pragma solidity ^0.4.18;
 
-/*
-* Add oraclize api used for call a function every 24h and to obtain data from external sources
-*
-*/
+/**
+ * Add oraclize api used for call a function every 24h and to obtain data from external sources
+ */
 import "github.com/oraclize/ethereum-api/oraclizeAPI.sol";
-/*
-* We will only have one instance of this contract, that will represent the compensation compensationChamber
-* All the contracts will be created using this one
-*/
+/**
+ * We will only have one instance of this contract, that will represent the compensation compensationChamber
+ * All the contracts will be created using this one
+ */
 contract compensationChamber
 {
-  /* Constants */
+  /**
+   * Constants
+   */
   uint private dayInSeconds = 86400; // Number of seconds in 24 h
 
-  /* Frome the moment that the CCP indicates the daily collateral that both counterparts
-  * Have to send to the chamber, they have 12 hours to pay it.
-  */
+  /**
+   * From the moment that the CCP indicates the daily collateral that both counterparts
+   * Have to send to the chamber, they have 12 hours to pay it.
+   */
   uint private timeToPayTheMargin = 43200;
 
-  /* Address of the compensation chamber and the market */
+  /**
+   * Address of the compensation chamber and the market
+   */
   address private owner;
   address private marketAddress;
 
-  /*
-  * Timestamp in seconds since epox indicating the date and time when the chamber will do the
-  * next asset valuation and will infrom of the daily marginal that the counterparts have to give to the chamber.
-  * Or to liquidate the operation if the contract has reached the maturity date.
-  */
+  /**
+   * Timestamp in seconds since epox indicating the date and time when the chamber will do the
+   * next asset valuation and will infrom of the daily marginal that the counterparts have to give to the chamber.
+   * Or to liquidate the operation if the contract has reached the maturity date.
+   */
   uint private nextRevisionTime;
   uint private deadlineForMarginalPayment;
   bool private marketAddressChange = true;
 
   /**
-  * Map an Ethereum account address with the corresponding clearing member contract address
-  */
+   * Map an Ethereum account address with the corresponding clearing member contract address
+   */
   maping (address => address) mapEtherAccountToContractAddress;
   address[] clearingMembersAddresses;
   address[] clearingMemberContractAddresses;
@@ -46,10 +50,10 @@ contract compensationChamber
     deadlineForMarginalPayment = nextRevisionTime + timeToPayTheMargin;
   }
 
-  /*
-  * Create the modifiers, this is a pattern that restricts the execution of some functions, read the content of modify the contract
-  * also allows to perfom timed transactions
-  */
+  /**
+   * Create the modifiers, this is a pattern that restricts the execution of some functions, read the content of modify the contract
+   * also allows to perfom timed transactions
+   */
   modifier onlyChamber
   {
     require(msg.sender == owner);
@@ -62,8 +66,8 @@ contract compensationChamber
   }
 
   /**
-  * Add functions of the smartcontract
-  */
+   * Add functions of the smartcontract
+   */
   function setMarketAddress(address _marketAddress) onlyChamber public
   {
     require(marketAddressChange);
@@ -75,10 +79,11 @@ contract compensationChamber
   {
     address contractAddress = mapEtherAccountToContractAddress[_clearingMemberAddress];
 
-    /* if the contract address is 0, means that he address conrresponds to a new clearing member
-    * so a new clearingMember contract have to be created.
-    * if the contract exist, revert the changes and send an error message
-    */
+    /**
+     * if the contract address is 0, means that he address conrresponds to a new clearing member
+     * so a new clearingMember contract have to be created.
+     * if the contract exist, revert the changes and send an error message
+     */
     if(contractAddress == 0)
     {
       mapEtherAccountToContractAddress[_clearingMemberAddress] = new clearingMember(_clearingMemberAddress);
@@ -109,17 +114,17 @@ contract compensationChamber
 contract clearingMember
 {
   /**
-  * Clearing member address
-  * Compensation Chamber address
-  * Array of addresses corresponding to the contracts that this clearing member have
-  */
+   * Clearing member address
+   * Compensation Chamber address
+   * Array of addresses corresponding to the contracts that this clearing member have
+   */
   address private memberAddress;
   address[] private assets;
   address private chamberAddress;
 
-  /*
-  * Add modifiers for this contract, one for the chamber and the other for the clearing memeber
-  */
+  /**
+   * Add modifiers for this contract, one for the chamber and the other for the clearing memeber
+   */
   modifier onlyChamber
   {
     require(msg.sender == chamberAddress);
