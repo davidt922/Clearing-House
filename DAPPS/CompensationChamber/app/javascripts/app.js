d3d1060fa2f4e61d2e2a552c8a6358d3741b7133
davidt922 <100371205@alumnos.uc3m.es>
2018-07-16 11:57:37 +0200

    Update web files from last project

--- DAPPS/CompensationChamber/app/javascripts/app.js
+++ DAPPS/CompensationChamber/app/javascripts/app.js
@@ -3,35 +3,58 @@ import "../stylesheets/app.css";
 
 // Import libraries we need.
 import { default as Web3} from 'web3';
-import { default as contract } from 'truffle-contract'
+import { default as contract } from 'truffle-contract';
 
 // Import our contract artifacts and turn them into usable abstractions.
-import metacoin_artifacts from '../../build/contracts/MetaCoin.json'
+import market_artifacts from '../../build/contracts/Market.json';
 
-// MetaCoin is our usable abstraction, which we'll use through the code below.
-var MetaCoin = contract(metacoin_artifacts);
+// Market is our usable abstraction, which we'll use thow the code below
+var Market = contract(market_artifacts);
 
-// The following code is simple to show off interacting with your contracts.
-// As your needs grow you will likely need to change its form and structure.
-// For application bootstrapping, check out window.addEventListener below.
 var accounts;
 var account;
 
+// Import libraries we need.
+$('.message a').click(function()
+{
+   $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+});
+
+$(document).ready(function () {
+
+        $('.register-form').hide();
+        $('.login-form').show();
+
+        $('#register').click(function () {
+            $('.register-form').hide();
+            $('.login-form').show
+            ();
+        });
+        $('#login').click(function () {
+            $('.register-form').show();
+            $('.login-form').hide();
+        });
+    });
+
 window.App = {
-  start: function() {
+  start: function()
+  {
     var self = this;
 
     // Bootstrap the MetaCoin abstraction for Use.
-    MetaCoin.setProvider(web3.currentProvider);
+    Market.setProvider(web3.currentProvider);
 
     // Get the initial account balance so it can be displayed.
-    web3.eth.getAccounts(function(err, accs) {
-      if (err != null) {
+    web3.eth.getAccounts(function(err, accs)
+    {
+      if (err != null)
+      {
         alert("There was an error fetching your accounts.");
         return;
       }
 
-      if (accs.length == 0) {
+      if (accs.length == 0)
+      {
         alert("Couldn't get any accounts! Make sure your Ethereum client is configured correctly.");
         return;
       }
@@ -39,18 +62,40 @@ window.App = {
       accounts = accs;
       account = accounts[0];
 
-      self.refreshBalance();
+      //self.refreshBalance();
     });
   },
 
-  setStatus: function(message) {
+  addCompensationMember: function(_name, _email)
+  {
+    var self = this;
+    var _market;
+    console.log("js functrion addCompensationMember executed");
+
+    Market.deployed().then(function(instance)
+    {
+      _market = instance;
+      console.log("Execute add clearing member");
+      return _market.addClearingMember(_name, _email, {from: account, gas: 39000000});
+    }).then(function(value)
+  {
+    alert("Done");
+    console.log(value);
+    //var result_element = document.getElementById("result");
+    //result_element.innerHTML = value.valueOf();
+  })
+},
+/*
+*/
+/*
+  setStatus: function(message)
+  {
     var status = document.getElementById("status");
     status.innerHTML = message;
   },
-
-  refreshBalance: function() {
+  refreshBalance: function()
+  {
     var self = this;
-
     var meta;
     MetaCoin.deployed().then(function(instance) {
       meta = instance;
@@ -63,15 +108,12 @@ window.App = {
       self.setStatus("Error getting balance; see log.");
     });
   },
-
-  sendCoin: function() {
+  sendCoin: function()
+  {
     var self = this;
-
     var amount = parseInt(document.getElementById("amount").value);
     var receiver = document.getElementById("receiver").value;
-
     this.setStatus("Initiating transaction... (please wait)");
-
     var meta;
     MetaCoin.deployed().then(function(instance) {
       meta = instance;
@@ -83,20 +125,34 @@ window.App = {
       console.log(e);
       self.setStatus("Error sending coin; see log.");
     });
-  }
+  }*/
 };
 
-window.addEventListener('load', function() {
+
+window.addEventListener('load', function()
+{
   // Checking if Web3 has been injected by the browser (Mist/MetaMask)
-  if (typeof web3 !== 'undefined') {
+  if (typeof web3 !== 'undefined')
+  {
     console.warn("Using web3 detected from external source. If you find that your accounts don't appear or you have 0 MetaCoin, ensure you've configured that source properly. If using MetaMask, see the following link. Feel free to delete this warning. :) http://truffleframework.com/tutorials/truffle-and-metamask")
     // Use Mist/MetaMask's provider
     window.web3 = new Web3(web3.currentProvider);
-  } else {
-    console.warn("No web3 detected. Falling back to http://127.0.0.1:9545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
+  }
+  else
+  {
+    console.warn("No web3 detected. Falling back to http://127.0.0.1:8545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
     // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-    window.web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:9545"));
+    window.web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:8545"));
   }
 
   App.start();
+  console.log("Is market deployed:");
+  console.log(Market.deployed());
+
+  document.getElementById("create").addEventListener("click", function()
+  {
+    var name = document.getElementById("name1");
+    var email = document.getElementById("email1");
+      App.addCompensationMember(name, email);
+  }, false);
 });
