e5d5b6c0d7f4ee1d011f8f92f6771a8d37f64fe2
davidt922 <100371205@alumnos.uc3m.es>
2018-07-20 15:03:23 +0200

    change gas and value when deploying Market contract

--- DAPPS/CompensationChamber/migrations/2_deploy_contracts.js
+++ DAPPS/CompensationChamber/migrations/2_deploy_contracts.js
@@ -4,5 +4,5 @@ module.exports = function(deployer, network, accounts)
 {
   // Deploys the OraclizeTest contract and funds it with 0.5 ETH
 // The contract needs a balance > 0 to communicate with Oraclize
-  deployer.deploy(Market1, 60000000, { from: accounts[9], value: 20000000000000000000 });
+  deployer.deploy(Market1, 80000000, { from: accounts[9], value: 25000000000000000000 });
 };
